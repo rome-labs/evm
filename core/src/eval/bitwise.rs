94b0a3ded78bb30993d99a69dc752a757e56b449
valiksinev <valiksinev@yahoo.com>
2025-05-06 11:56:49 +0400

    apply HAL-09

--- core/src/eval/bitwise.rs
+++ core/src/eval/bitwise.rs
@@ -57,8 +57,7 @@ pub fn shl(shift: U256, value: U256) -> U256 {
 	if value == U256::zero() || shift >= U256::from(256) {
 		U256::zero()
 	} else {
-		let shift: u64 = shift.as_u64();
-		value << shift as usize
+		value << shift.as_usize()
 	}
 }
 
@@ -66,8 +65,7 @@ pub fn shr(shift: U256, value: U256) -> U256 {
 	if value == U256::zero() || shift >= U256::from(256) {
 		U256::zero()
 	} else {
-		let shift: u64 = shift.as_u64();
-		value >> shift as usize
+		value >> shift.as_usize()
 	}
 }
 
@@ -83,12 +81,11 @@ pub fn sar(shift: U256, value: U256) -> U256 {
 			Sign::Minus => I256(Sign::Minus, U256::one()).into(),
 		}
 	} else {
-		let shift: u64 = shift.as_u64();
 
 		match value.0 {
-			Sign::Plus | Sign::NoSign => value.1 >> shift as usize,
+			Sign::Plus | Sign::NoSign => value.1 >> shift.as_usize(),
 			Sign::Minus => {
-				let shifted = ((value.1.overflowing_sub(U256::one()).0) >> shift as usize)
+				let shifted = ((value.1.overflowing_sub(U256::one()).0) >> shift.as_usize())
 					.overflowing_add(U256::one()).0;
 				I256(Sign::Minus, shifted).into()
 			}
